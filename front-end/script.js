5b10d859c5d29c54abe90482216d04b410fbb3b5
Rom1v <rom1.verfaillie@gmail.com>
2021-07-24 10:37:55 +0200

    ajout code api

--- front-end/script.js
+++ front-end/script.js
@@ -1,3 +1,26 @@
-fetch('http://localhost:3000/api/teddies')
-  .then(res => res.json())
-  .then(data => console.log(data))
+let teddyData = [];
+
+const fetchTeddy = async () => {
+  await fetch("http://localhost:3000/api/teddies")
+    .then((res) => res.json())
+    .then((data) => (teddyData = data));
+
+  console.log(teddyData);
+};
+
+const teddyDisplay = async () => {
+  await fetchTeddy();
+  document.getElementById("bloc_products").innerHTML = teddyData
+    .map(
+      (teddy) => `<div class="formatting_products">
+    <img class="img_product" src=${teddy.imageUrl} alt="${teddy.name}">
+    <div class="nameandprice">
+    <h3>${teddy.name}</h3>
+    <p>${teddy.price / 100},00 €</p>
+    </div>
+    <p>${teddy.description}</p>
+    </div>`
+    )
+    .join("");
+};
+teddyDisplay();
